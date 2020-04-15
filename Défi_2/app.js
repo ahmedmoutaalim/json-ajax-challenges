ca6bac75c722d3e6d221318b38f3541c7c662238
Omar <Omar@omar.com>
2020-04-15 23:52:01 +0100

    done défi 2

--- Défi_2/app.js
+++ Défi_2/app.js
@@ -1,11 +1,11 @@
-var Coustomer = document.getElementById('button1');
-var Coustomers = document.getElementById('button2');
+var Customer = document.getElementById('button1');
+var Customers = document.getElementById('button2');
 
-var Coust = document.getElementById('customer');
-var Cousts = document.getElementById('customers');
+var Cust = document.getElementById('customer');
+var Custs = document.getElementById('customers');
 
 
-Coustomer.addEventListener('click',function(){
+Customer.addEventListener('click',function(){
 
 var xReq = new XMLHttpRequest();
 xReq.open('GET','customer.json');
@@ -14,10 +14,10 @@ xReq.onload = function(){
 
     var xData = JSON.parse(xReq.responseText);
 
-    Coust.innerHTML  =  "id :" + xData.id + "<br>"
+    Cust.innerHTML  =  "id :" + xData.id + "<br>"
                          +  "name :" + xData.name + "<br>" 
                          + " company :"  + xData.company +"<br>" 
-                         + " phone :" +xData.phone;
+                         + " phone :" +xData.phone + "<hr>";
 }
 
 
@@ -29,12 +29,24 @@ xReq.send();
 
 
 
-Coustomers.addEventListener('click',function(){
+Customers.addEventListener('click',function(){
 
+    var xReq = new XMLHttpRequest();
+    xReq.open('GET','customers.json');
 
+    xReq.onload= function(){
 
+    var xData = JSON.parse(xReq.responseText);
+    var output = "";
+    for(i=0 ; i<xData.length ; i++){
 
-
-
+         output += "id :" + xData[i].id + "<br>" 
+                   + "name :" + xData[i].name + "<br>" 
+                   +"company :"+ xData[i].company+"<br>" 
+                   + "phone :"+ xData[i].phone +"<hr>";
+        }
+    Custs.innerHTML = output;
+    }
+    xReq.send();
 });
 
